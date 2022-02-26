244d957146f36a24dae50ce33200d3ccf0a2e607
kgdmzz <43299860+kgdmzz@users.noreply.github.com>
2022-02-26 08:33:50 -0800

    Update c9installer.sh

--- c9installer.sh
+++ c9installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt-get update -y
+sudo apt-get install nodejs -y
 sudo apt-get install curl -y 
 sudo apt-get install python -y
 sudo apt-get install build-essential -y
 sudo apt-get install git -y
 curl -sL https://deb.nodesource.com/setup_8.x -o ~/nodesource_setup.sh
 sudo bash ~/nodesource_setup.sh
-sudo apt-get install nodejs -y
 sudo ufw allow 8080
 git clone https://github.com/c9/core.git ~/c9sdk
 sudo bash ~/c9sdk/scripts/install-sdk.sh
